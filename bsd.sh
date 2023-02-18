decc94de91019ef39c6656d40da1dc77afe75cb4
Odexia <125573921+Odexia@users.noreply.github.com>
2023-02-18 11:07:42 +0100

    Update bsd.sh

--- bsd.sh
+++ bsd.sh
@@ -38,6 +38,7 @@ echo ""
 #echo "MAKE_JOBS_NUMBER?=16" >> /etc/make.conf
 #echo "OPTIONS_SET=OPTIMIZED_CFLAGS CPUFLAGS" >> /etc/make.conf
 
+
 ## INSTALLS BASE DESKTOP AND CORE UTILS
 echo "====================================="
 echo "= Installing XFCE AND CORE UTILS... ="
@@ -62,6 +63,7 @@ then
     echo "$user enabled"
 else fi
 
+
 ## INSTALLS BASE DESKTOP AND CORE UTILS
 echo "=============================="
 echo "= Installing NVIDIA UTILS... ="
@@ -70,13 +72,13 @@ echo ""
 pkg install -y nvidia-driver nvidia-settings nvidia-xconfig linux-nvidia-libs
 nvidia-xconfig
 
+
 ## ENABLES LINUX COMPAT LAYER
 echo "=================================="
 echo "= Enabling Linux compat layer... ="
 echo "=================================="
 echo ""
 kldload linux.ko
-sysrc linux_enable="YES"
 echo ""
 
 
@@ -85,34 +87,16 @@ echo "============================"
 echo "= Installing MORE UTILS... ="
 echo "============================"
 echo ""
-pkg install -y vlc firefox vscode suyimazu linux-steam-utils
+pkg install -y vlc firefox vscode suyimazu linux-steam-utils htop xarchiver 7-zip v4l-utils v4l_compat sctd sudo wget atril-lite   
 
 
-pkg install -y nano sudo wget htop xarchiver unzip 
-pkg install -y gnome-keyring xfce4-screenshooter-plugin shotwell atril-lite gnome-font-viewer qjackctl  
-pkg install -y baobab v4l-utils v4l_compat webcamd pwcview sctd brut deadbeef
 
 ## INSTALLS AUTOMOUNT AND FILESYSTEM SUPPORT
+echo "========================="
+echo "= Enabling automount... ="
+echo "========================="
+pkg install -y automount exfat-utils fusefs-exfat fusefs-ntfs fusefs-ext2 fusefs-hfsfuse fusefs-lkl fusefs-smbnetfs dsbmd dsbmc
 echo ""
-echo "Enabling automount..."
-echo ""
-pkg install -y automount exfat-utils fusefs-exfat fusefs-ntfs fusefs-ext2 fusefs-hfsfuse fusefs-lkl fusefs-simple-mtpfs dsbmd dsbmc
-sysrc dsbmd_enable=YES
-echo ""
-
-## ENABLES BASIC SYSTEM SERVICES
-echo "==================================="
-echo "= Enabling basic services rc.conf ="
-echo "==================================="
-echo ""
-sysrc moused_enable="YES"
-sysrc dbus_enable="YES"
-sysrc sddm_enable="YES"
-sysrc update_motd="NO"
-sysrc rc_startmsgs="NO"
-echo ""
-
-
 
 ## CONFIGURES AUTOMOUNT FOR THE REGULAR DESKTOP USER
 touch /usr/local/etc/automount.conf
@@ -126,23 +110,59 @@ echo "perm    /dev/da0        0666" >> /etc/devfs.conf
 echo "perm    /dev/da1        0666" >> /etc/devfs.conf
 echo "perm    /dev/da2        0666" >> /etc/devfs.conf
 echo "perm    /dev/da3        0666" >> /etc/devfs.conf
-echo "perm    /dev/video0     0666" >> /etc/devfs.conf
 echo ""
+
 
 ## ADDS USER TO CORE GROUPS
-echo "Adding $user to video/realtime/wheel/operator groups"
-pw usermod $user -G video
-#pw usermod $user -G realtime
-pw usermod $user -G wheel
-pw usermod $user -G operator
-pw usermod $user -G network
-#pw usermod $user -G webcamd
+echo "======================================================="
+echo "= Adding $user to video/realtime/wheel/operator groups ="
+echo "======================================================="
+pw groupmod video -m $user
+pw groupmod realtime -m $user
+pw groupmod operator -m $user
+pw groupmod wheel -m $user
+pw groupmod network -m $user
 echo ""
 
+
 ## ADDS USER TO SUDOERS
-echo "Adding $user to sudo"
+echo "======================="
+echo "= Adding $user to sudo ="
+echo "======================="
 echo "$user ALL=(ALL:ALL) ALL" >> /usr/local/etc/sudoers
 echo ""
+
+
+## ENABLES BASIC SYSTEM SERVICES
+echo "==================================="
+echo "= Enabling basic services rc.conf ="
+echo "==================================="
+echo ""
+sysrc moused_enable="YES"
+sysrc dbus_enable="YES"
+sysrc sddm_enable="YES" #Login manager
+sysrc linux_enable="YES" #Kernel linux load
+sysrc dsbmd_enable="YES" #Automount media
+sysrc update_motd="NO"
+sysrc rc_startmsgs="NO"
+sysrc ntpd_enable="YES"
+sysrc ntpdate_enable="YES"
+sysrc syslogd_flags="-ss"
+sysrc dumpdev="NO"
+echo ""
+
+## CONFIGURES MORE CORE SYSTEM SERVICES
+echo "=========================================="
+echo "= Enabling additional system services... ="
+echo "=========================================="
+echo ""
+sysrc clear_tmp_enable="YES"
+sysrc sendmail_enable="NONE"
+sysrc sendmail_msp_queue_enable="NO"
+sysrc sendmail_outbound_enable="NO"
+sysrc sendmail_submit_enable="NO"
+echo ""
+
 
 
 ## FreeBSD SYSTEM TUNING FOR BEST DESKTOP EXPERIENCE
@@ -157,42 +177,23 @@ fetch https://raw.githubusercontent.com/Wamphyre/BSD-XFCE/main/devfs.rules
 cd /boot/ && fetch https://raw.githubusercontent.com/Wamphyre/BSD-XFCE/main/loader.conf
 sysrc devfs_system_ruleset="desktop"
 cd
+
+
+## Initializing FW
+echo "=========================="
+echo "= Configure firewall ... ="
+echo "=========================="
 touch /etc/pf.conf
 echo 'block in all' >> /etc/pf.conf
 echo 'pass out all keep state' >> /etc/pf.conf
-
-## CONFIGURES MORE CORE SYSTEM SERVICES
-echo "Enabling additional system services..."
-echo ""
 sysrc pf_enable="YES"
 sysrc pf_rules="/etc/pf.conf" 
 sysrc pf_flags=""
 sysrc pflog_enable="YES"
 sysrc pflog_logfile="/var/log/pflog"
 sysrc pflog_flags=""
-sysrc ntpd_enable="YES"
-sysrc ntpdate_enable="YES"
-sysrc powerd_enable="YES"
-sysrc powerd_flags="-n adaptive -a hiadaptive -m 2200 -M 4000"
-sysrc performance_cx_lowest="C1"
-sysrc economy_cx_lowest="Cmax"
-sysrc clear_tmp_enable="YES"
-sysrc syslogd_flags="-ss"
-sysrc sendmail_enable="NONE"
-sysrc sendmail_msp_queue_enable="NO"
-sysrc sendmail_outbound_enable="NO"
-sysrc sendmail_submit_enable="NO"
-sysrc dumpdev="NO"
-sysrc webcamd_enable="YES"
-##Use your own USB port for webcam
-sysrc webcamd_0_flags="-d ugen2.2" 
-sysrc jackd_enable="YES"
-sysrc jackd_user="$user"
-sysrc jackd_rtprio="YES"
-## Change JACK /dev/dsp7 by your own interfaces
-sysrc jackd_args="-r -doss -r48000 -p512 -n1 -w16 \
-            --capture /dev/dsp7 --playback /dev/dsp7"
-echo ""
+
+
 
 ## UPDATES CPU MICROCODE
 echo "============================="
@@ -212,6 +213,31 @@ echo "======================"
 pkg clean -y
 pkg autoremove -y
 echo ""
+
+
+echo "================================"
+echo "= Configuration for laptop ... ="
+echo "================================"
+echo ; read -p "Want to enable laptop optimization? (yes/no): " Y;
+echo ""
+if [ "$Y" = "yes" ]
+then
+    pkg install -y webcamd pwcview qjackctl
+    pw groupmod webcamd -m $user
+    echo "perm    /dev/video0     0666" >> /etc/devfs.conf
+    sysrc powerd_enable="YES"
+    #sysrc powerd_flags="-n adaptive -a hiadaptive -m 2200 -M 4000"
+    sysrc performance_cx_lowest="C1"
+    sysrc economy_cx_lowest="Cmax"
+    sysrc webcamd_enable="YES"
+    ##Use your own USB port for webcam
+    #sysrc webcamd_0_flags="-d ugen2.2" 
+    sysrc jackd_enable="YES"
+    sysrc jackd_user="$user"
+    sysrc jackd_rtprio="YES"
+    ## Change JACK /dev/dsp7 by your own interfaces
+    #sysrc jackd_args="-r -doss -r48000 -p512 -n1 -w16 --capture /dev/dsp7 --playback /dev/dsp7"
+else fi
 
 ## DONE, REBOOT
 echo "===================================="
